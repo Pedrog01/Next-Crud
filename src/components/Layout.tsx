e26df139b59b9926d9f81b41452854c377e933f0
Pedro <pedro7109.j@gmail.com>
2022-11-07 10:38:25 -0300

    components Layout and Title

---
+++ src/components/Layout.tsx
@@ -0,0 +1,21 @@
+import Titulo from "./Titulo"
+
+interface LayoutProps{
+    titulo : string
+    children : any
+}
+
+export default function Layout(props : LayoutProps){
+return (
+    <div className={`
+        flex flex-color w-2/3s
+        bg-white text-gray-800 rounded-md
+    `}>
+        <Titulo>{props.titulo}</Titulo>
+        <div className="p-6">
+        {props.children}
+        </div>
+    </div>
+)
+
+}
